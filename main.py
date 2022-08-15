7ce983debe90a53401499b48ca6d7b1b77c9e49c
Dmitry Kozmin <kozmin4ik@gmail.com>
2022-08-15 17:00:04 +0300

    made some changes

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from task import read_json
 example_array = ['Phone : Number', 'Name : Title', 'Name : First Name', 'Name : Middle Name', 'Name : Last Name', 'Name : Full Name']
 
 if __name__ == "__main__":
-    file_name = input('Enter path to the file: ')
-    read_json(file_name, example_array)
+    input_data = input('Enter path to the file: ')
+    with open(input_data, 'r') as f:
+        data = f.read().splitlines()
+    print(read_json(input_data=data, task_array=example_array))
--- task.py
+++ task.py
@@ -1,30 +1,29 @@
 import json
 
 
-def read_json(file_name, task_array):
-    with open(file_name, 'r') as f:
-        data = f.read().splitlines()
-        result_array = []
-        print(* task_array, sep=', ')
-        for i in data:
-            i = i.split(';')
-            i[3] = i[3].replace(f"'", '"')
-            smth = i[3]
-            data_line = json.loads(smth)
-            for data in data_line:
-                try:
-                    answer = data['peer_response_with_source']['Payload']['All Categories']['Answers']
-                    result_array.append(f'"{i[0]}"')
-                    for name in answer[0]['Answer']:
-                        for task_data in task_array:
-                            try:
-                                if name['Key'] == task_data:
-                                    result_array.append(f'"{name["Value"]}"')
-                            except KeyError:
-                                result_array.append('" "')
-                except KeyError:
-                    continue
+def read_json(input_data, task_array):
+    output_array = []
+    result_array = []
+    output_array.append(task_array)
+    for i in input_data:
+        i = i.split(';')
+        i[3] = i[3].replace(f"'", '"')
+        smth = i[3]
+        data_line = json.loads(smth)
+        for data in data_line:
+            try:
+                answer = data['peer_response_with_source']['Payload']['All Categories']['Answers']
+                result_array.append(f'"{i[0]}"')
+                for name in answer[0]['Answer']:
+                    for task_data in task_array:
+                        try:
+                            if name['Key'] == task_data:
+                                result_array.append(f'"{name["Value"]}"')
+                        except KeyError:
+                            result_array.append('" "')
+            except KeyError:
+                continue
 
-            print(* result_array, sep=', ')
+            output_array.append(result_array)
             result_array = []
-
+    return output_array
