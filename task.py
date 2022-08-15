c0a42d97e80d27e72d2d0de24599cbbbc99fb9fc
Dmitry Kozmin <kozmin4ik@gmail.com>
2022-08-15 15:42:06 +0300

    made some changes

--- task.py
+++ task.py
@@ -1,11 +1,11 @@
 import json
 
 
-def read_json(file_name, example_array):
+def read_json(file_name, task_array):
     with open(file_name, 'r') as f:
         data = f.read().splitlines()
         result_array = []
-        print(* example_array, sep=', ')
+        print(* task_array, sep=', ')
         for i in data:
             i = i.split(';')
             i[3] = i[3].replace(f"'", '"')
@@ -16,9 +16,9 @@ def read_json(file_name, example_array):
                     answer = data['peer_response_with_source']['Payload']['All Categories']['Answers']
                     result_array.append(f'"{i[0]}"')
                     for name in answer[0]['Answer']:
-                        for ex_data in example_array:
+                        for task_data in task_array:
                             try:
-                                if name['Key'] == ex_data:
+                                if name['Key'] == task_data:
                                     result_array.append(f'"{name["Value"]}"')
                             except KeyError:
                                 result_array.append('" "')
